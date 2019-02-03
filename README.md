ff7cd749be4cbe9ab90c74dc3487c111a624187e
mp0liiu <raian@reeshome.org>
2019-02-03 12:28:49 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Acme::PsychoPass - It's new $module
+
+# SYNOPSIS
+
+    use Acme::PsychoPass;
+
+# DESCRIPTION
+
+Acme::PsychoPass is ...
+
+# LICENSE
+
+Copyright (C) mp0liiu.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+mp0liiu <raian@reeshome.org>
---
+++ lib/Acme/PsychoPass.pm
@@ -0,0 +1,39 @@
+package Acme::PsychoPass;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Acme::PsychoPass - It's new $module
+
+=head1 SYNOPSIS
+
+    use Acme::PsychoPass;
+
+=head1 DESCRIPTION
+
+Acme::PsychoPass is ...
+
+=head1 LICENSE
+
+Copyright (C) mp0liiu.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+mp0liiu E<lt>raian@reeshome.orgE<gt>
+
+=cut
+
